6da334d48418350e0272d2460e818a957c93f06a
Jim Yeung <qzy114@gmail.com>
2023-09-27 22:06:35 -0400

    withdraw funds

--- bank-account/account.cmd/src/main/java/com/techbank/account/cmd/api/controller/WithdrawFundsController.java
+++ bank-account/account.cmd/src/main/java/com/techbank/account/cmd/api/controller/WithdrawFundsController.java
@@ -1,7 +1,6 @@
 package com.techbank.account.cmd.api.controller;
 
 import java.text.MessageFormat;
-import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -14,32 +13,32 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.techbank.account.cmd.api.commands.DepositFundsCommand;
+import com.techbank.account.cmd.api.commands.WithdrawFundsCommand;
 import com.techbank.account.common.dto.BaseResponse;
 import com.techbank.cqrs.core.infrastructure.CommandDispatcher;
 
 @RestController
-@RequestMapping(path = "/api/v1/depositFunds")
-public class DepositFundsController {
+@RequestMapping(path = "/api/v1/withdrawFunds")
+public class WithdrawFundsController {
 
-	private final Logger logger = Logger.getLogger(DepositFundsController.class.getName());
+	private final Logger logger = Logger.getLogger(WithdrawFundsController.class.getName());
 
 	@Autowired
 	private CommandDispatcher commandDispatcher;
 
 	@PutMapping(path = "/{id}")
-	public ResponseEntity<BaseResponse> depositFunds(@PathVariable String id, @RequestBody DepositFundsCommand command) {
+	public ResponseEntity<BaseResponse> withdrawFunds(@PathVariable String id, @RequestBody WithdrawFundsCommand command) {
 		try {
 			command.setId(id);
 			commandDispatcher.send(command);
-			logger.log(Level.INFO, "Bank account created " + id);
-			return new ResponseEntity<>(new BaseResponse("Funds deposit request completed successfully"),
+			logger.log(Level.INFO, "withraw funds with id  " + id);
+			return new ResponseEntity<>(new BaseResponse("Funds withdraw request completed successfully"),
 					HttpStatus.OK);
 		} catch (IllegalStateException e) {
 			logger.log(Level.WARNING, MessageFormat.format("Client made a bad reqeust {0}", e.toString()));
 			return new ResponseEntity<>(new BaseResponse(e.toString()), HttpStatus.BAD_REQUEST);
 		} catch (Exception e) {
-			logger.log(Level.SEVERE, MessageFormat.format("Error while process deposit fund request for id = {0}", id));
+			logger.log(Level.SEVERE, MessageFormat.format("Error while process withdraw fund request for id = {0}", id));
 			return new ResponseEntity<>(new BaseResponse(e.toString()), HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
